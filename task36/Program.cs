930d99ec00516b097c6082c35cdd3c79c9e3a0f6
timass222 <tima.sidorenko38@gmail.com>
2022-10-19 21:01:57 +0300

    DZ5

---
+++ task36/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 36: Задайте одномерный маcсив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int SumIndexNum (int[] array)
+{
+    int sum = default;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (i % 2 == 1) sum = sum + array[i];
+    }
+    return sum;
+}
+
+
+int[] arr = CreateArrayRndInt (6, -10, 10);
+PrintArray(arr);
+int sumIndexNum = SumIndexNum(arr);
+Console.WriteLine(sumIndexNum);
---
+++ task38/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+
+double[] CreateArrRndInt(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Math.Round (rnd.NextDouble() * (max - min) + min, 1);
+    }
+    return arr;
+}
+
+void PrintArr (double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double GetMinM (double[] arr)
+{
+    double minRes = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < minRes) minRes = arr[i];
+    }
+    return minRes;
+}
+
+double GetMaxM (double[] arr)
+{
+    double maxRes =  arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > maxRes) maxRes = arr[i];
+    }
+
+    return maxRes;
+}
+
+double GetDiffMaxMin (double[] arr)
+{
+    double maxM = GetMaxM(arr);
+    double minM = GetMinM(arr);
+    double res = Math.Round(maxM - minM, 1);
+    return res;
+}
+
+
+double[] arr = CreateArrRndInt(5, -10, 10);
+PrintArr(arr);
+
+double diffMaxMinM = GetDiffMaxMin(arr);
+Console.WriteLine(diffMaxMinM);
