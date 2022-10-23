3ce7484477d43b9a18aa0b3d9eec3433f7aebd30
timass222 <tima.sidorenko38@gmail.com>
2022-10-23 23:01:11 +0300

    DZ6

---
+++ task41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+
+Console.WriteLine("Введите количество чисел: ");
+int quantNum = Convert.ToInt32(Console.ReadLine());
+
+int QuantPositNum (int number, int quant)
+{
+    int counter = default;
+    for (int i = 0; i < quant; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > number) counter++;
+    }
+    return counter;
+}
+
+int quantPositNum = QuantPositNum (0, quantNum);
+Console.WriteLine($"Количество чисел больше 0 => {quantPositNum}");
+
---
+++ task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+Console.WriteLine("Задайте значение b1: ");
+double B1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Задайте значение k1: ");
+double K1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Задайте значение b2: ");
+double B2 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Задайте значение k2: ");
+double K2 = Convert.ToDouble(Console.ReadLine());
+
+void IntersPoint (double b1, double b2, double k1, double k2)
+{
+    if (k1 == k2) Console.WriteLine("Прямые параллельны");
+
+    else {
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения прямых ({x}, {y})");
+    }
+
+}
+
+IntersPoint(B1, B2, K1, K2);
